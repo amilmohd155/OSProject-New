071ad97dfb0bb6327db203a5fed964c90393073f
amilmohd155 <32008354+amilmohd155@users.noreply.github.com>
2019-04-17 00:57:03 +0530

    Update osProject.cpp

--- osProject.cpp
+++ osProject.cpp
@@ -2,82 +2,85 @@
 using namespace std;
 class process{
 	public:
-		int start,end,priority,id;		
+		int start,burst,id,wait,tat;
+		float priority;
+    process() {
+    priority=0;}
 };
-bool comp(process p1,process p2)
+bool arrivalTime(process p1,process p2)
 {
-	return (p1.start<p2.start) || (p1.start==p2.start && p1.priority<p2.priority);
+	return p1.start < p2.start;
 }
-bool comp2(process p1,process p2)
-{
-	return  p1.priority<p2.priority;
+
+bool priority(process p1, process p2) {
+    return p1.priority < p2.priority;
 }
+
 
 int main()
 {
 	int n;
+	n = 4;
 	cout<<"Enter the number of processes\n";
 	cin>>n;
-	
+
 	process *p=new process[n];
-	
-	vector<process>q;
-	
-	int max_start=0;
+
 	for(int i=0;i<n;i++)
 	{
-		cout<<"Start and End time of Process "<<i+1<<"\n";
-		cin>>p[i].start>>p[i].end;
+		cout<<"arrival and burst time of Process "<<i+1<<"\n";
+		cin>>p[i].start>>p[i].burst;
 		p[i].id=i+1;
-		p[i].priority=p[i].end-p[i].start;
-		
-		if(p[i].start>max_start)
-		max_start=p[i].start;
-		
-		q.push_back(p[i]);
-	}
-	sort(p,p+n,comp);	
-	
-	int j=0;
-	int time=0;
-	
-	while(j<n)
-	{
-			int turn_around=p[j].end-p[j].start;
-		if(time>=p[j].start)
-		{
-			process pr=p[j];
-			
-			
-			
-			cout<<"Process "<<p[j].id<<" is getting executed from time "<<time<<" to "<<time+turn_around<<"\n";
-			j++;
-		
-		
-		
-			for(int k=0;k<turn_around;k++)
-			{
-				for(int i=j+1;i<n;i++)
-				{
-					if(time>p[i].start)
-					p[i].priority--;
-				}
-				time++;
-			}
-		}
-		else
-		{
-			cout<<"Process "<<p[j].id<<" is getting executed from time "<<time<<" to "<<time+turn_around<<"\n";
-			j++;
-		}
-		
-		sort(p+j,p+n,comp);
-		
-		if(time>max_start)
-		sort(p+j,p+n,comp);
-	}
 
-	
-	
-	
+	}
+	sort(p, p+n, arrivalTime);
+
+	int j = 1;
+	int k = 0;
+	int time = 0;
+
+    time += p[0].burst;
+    p[0].tat = time - p[0].start;
+    p[0].wait = p[0].tat - p[0].burst;
+    p[0].priority = 0;
+
+
+    while(j < n) {
+
+        process pr = p[j];
+
+        if(time >= pr.start) {
+
+            for(int k = j; k < n; ++k) {
+                p[k].wait = time - p[k].start;
+                p[k].priority = 1 + ((float)p[k].wait / (float)p[k].burst);
+
+            }
+
+            int bigIndex=max_element(p+j,p+n,priority)-p;
+            time += p[bigIndex].burst;
+            p[bigIndex].tat = time - p[bigIndex].start;
+            p[bigIndex].wait = p[bigIndex].tat - p[bigIndex].burst;
+
+
+            j++;
+            continue;
+
+        }
+        else {
+            ++j;
+            continue;
+        }
+
+    }
+    cout<<"Time"<<time;
+    int sumTat = 0;
+    int sumWt = 0;
+    for(int i = 0; i < n; ++i) {
+        sumTat += p[i].tat;
+        sumWt += p[i].wait;
+    }
+    cout<<"Average TurnAroundTime::"<<(float)sumTat/(float)n<<endl;
+    cout<<"Average WaitingTime::"<<(float)sumWt/(float)n<<endl;
+
 }
